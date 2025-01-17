1120b6ad6ebf50605fc835a6ac61ded3f08c0802
shehabmohamed99 <88060596+shehabmohamed99@users.noreply.github.com>
2025-01-17 11:02:42 +0200

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -9,8 +9,8 @@ db_host = os.getenv('DB_HOST')
 db_client = os.getenv('DB_CLIENT')
 db_users = os.getenv('DB_USERS')
 db_quizzes = os.getenv('DB_QUIZZES')
-host = os.getenv('APP_HOST')  
-port = os.getenv('APP_PORT')
+host = os.getenv('APP_HOST', '0.0.0.0')  
+port = os.getenv('APP_PORT', '8080')
 
 
 app.secret_key = os.urandom(24)
